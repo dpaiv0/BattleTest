16f6a0a3a4082bfb5bb6cda31dffdcfbafaec709
David Paiva <hello@c2g.space>
2019-12-12 00:42:53 +0000

    Add files via upload

---
+++ BattleTest/Form1.Designer.vb
@@ -0,0 +1,245 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ProgressBar2 = New System.Windows.Forms.ProgressBar()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.ProgressBar3 = New System.Windows.Forms.ProgressBar()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(82, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Opponent's HP:"
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(99, 9)
+        Me.ProgressBar1.Maximum = 0
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(118, 13)
+        Me.ProgressBar1.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(223, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(13, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "0"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(305, 326)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(580, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(13, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "0"
+        '
+        'ProgressBar2
+        '
+        Me.ProgressBar2.Location = New System.Drawing.Point(456, 9)
+        Me.ProgressBar2.Maximum = 0
+        Me.ProgressBar2.Name = "ProgressBar2"
+        Me.ProgressBar2.Size = New System.Drawing.Size(118, 13)
+        Me.ProgressBar2.TabIndex = 5
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(400, 9)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(50, 13)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Your HP:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(611, 9)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(42, 13)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "Round:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(659, 9)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(13, 13)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "0"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(580, 28)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(13, 13)
+        Me.Label7.TabIndex = 11
+        Me.Label7.Text = "0"
+        '
+        'ProgressBar3
+        '
+        Me.ProgressBar3.Location = New System.Drawing.Point(456, 28)
+        Me.ProgressBar3.Maximum = 0
+        Me.ProgressBar3.Name = "ProgressBar3"
+        Me.ProgressBar3.Size = New System.Drawing.Size(118, 13)
+        Me.ProgressBar3.TabIndex = 10
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(400, 28)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(51, 13)
+        Me.Label8.TabIndex = 9
+        Me.Label8.Text = "Your MP:"
+        '
+        'Button2
+        '
+        Me.Button2.Enabled = False
+        Me.Button2.Location = New System.Drawing.Point(403, 74)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(184, 30)
+        Me.Button2.TabIndex = 12
+        Me.Button2.Text = "Punch (0 MP, 40 DMG)"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Enabled = False
+        Me.Button3.Location = New System.Drawing.Point(403, 110)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(184, 30)
+        Me.Button3.TabIndex = 13
+        Me.Button3.Text = "Chidori (30MP, 90 DMG)"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Enabled = False
+        Me.Button4.Location = New System.Drawing.Point(403, 146)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(184, 30)
+        Me.Button4.TabIndex = 14
+        Me.Button4.Text = "Lightning Strike (50MP, 110 DMG)"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Enabled = False
+        Me.Button5.Location = New System.Drawing.Point(403, 182)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(184, 30)
+        Me.Button5.TabIndex = 15
+        Me.Button5.Text = "Heal (20MP, 50 HP)"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(12, 79)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(0, 25)
+        Me.Label9.TabIndex = 16
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(684, 361)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.ProgressBar3)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ProgressBar2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.Text = "BattleTest"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ProgressBar2 As ProgressBar
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents ProgressBar3 As ProgressBar
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Label9 As Label
+End Class
---
+++ BattleTest/Form1.vb
@@ -0,0 +1,268 @@
+﻿Public Class Form1
+    Dim rnd As New Random
+    Dim opponentHp As Integer = rnd.Next(800, 1301)
+    Dim characterHp As Integer = 900
+    Dim characterMp As Integer = 100
+    Dim round As Integer = 1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Opponent's HP
+        Label2.Text = opponentHp
+        ProgressBar1.Maximum = opponentHp
+        ProgressBar1.Value = opponentHp
+
+        ' User's HP
+        Label3.Text = characterHp
+        ProgressBar2.Maximum = characterHp
+        ProgressBar2.Value = characterHp
+
+        ' User's MP
+        Label7.Text = characterMp
+        ProgressBar3.Maximum = characterMp
+        ProgressBar3.Value = characterMp
+
+        ' Set the round value
+        Label6.Text = round
+
+        ' Disable start button & enable skill buttons
+        Button1.Enabled = False
+        EnableSkillButtons()
+        Button5.Enabled = False
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim criticalChance As Integer = rnd.Next(1, 5)
+
+        If criticalChance = 4 Then
+            If (Label2.Text - 80) <= 0 Then
+                Label2.Text = 0
+                ProgressBar1.Value = 0
+                EndBattle("Character")
+                Exit Sub
+            End If
+
+            ' 2x value of attack
+            Label2.Text -= 80
+            ProgressBar1.Value -= 80
+        Else
+            If (Label2.Text - 40) <= 0 Then
+                Label2.Text = 0
+                ProgressBar1.Value = 0
+                EndBattle("Character")
+                Exit Sub
+            End If
+
+            Label2.Text -= 40
+            ProgressBar1.Value -= 40
+        End If
+
+        Label7.Text += 20
+        ProgressBar3.Maximum += 20
+        ProgressBar3.Value += 20
+
+        DisableSkillButtons()
+        DoEnemyAttack()
+    End Sub
+
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim criticalChance As Integer = rnd.Next(1, 5)
+
+        If criticalChance = 4 Then
+            If (Label2.Text - 180) <= 0 Then
+                Label2.Text = 0
+                ProgressBar1.Value = 0
+                EndBattle("Character")
+                Exit Sub
+            End If
+
+            ' 2x value of attack
+            Label2.Text -= 180
+            ProgressBar1.Value -= 180
+        Else
+            If (Label2.Text - 90) <= 0 Then
+                Label2.Text = 0
+                ProgressBar1.Value = 0
+                EndBattle("Character")
+                Exit Sub
+            End If
+
+            Label2.Text -= 90
+            ProgressBar1.Value -= 90
+        End If
+
+        Label7.Text -= 30
+        ProgressBar3.Value -= 30
+
+        Label7.Text += 20
+        ProgressBar3.Maximum += 20
+        ProgressBar3.Value += 20
+
+        DisableSkillButtons()
+        DoEnemyAttack()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim criticalChance As Integer = rnd.Next(1, 5)
+
+        If criticalChance = 4 Then
+            If (Label2.Text - 220) <= 0 Then
+                Label2.Text = 0
+                ProgressBar1.Value = 0
+                EndBattle("Character")
+                Exit Sub
+            End If
+
+            ' 2x value of attack
+            Label2.Text -= 220
+            ProgressBar1.Value -= 220
+        Else
+            If (Label2.Text - 110) <= 0 Then
+                Label2.Text = 0
+                ProgressBar1.Value = 0
+                EndBattle("Character")
+                Exit Sub
+            End If
+
+            Label2.Text -= 110
+            ProgressBar1.Value -= 110
+        End If
+
+        Label7.Text -= 50
+        ProgressBar3.Value -= 50
+
+        Label7.Text += 20
+        ProgressBar3.Maximum += 20
+        ProgressBar3.Value += 20
+
+        DisableSkillButtons()
+        DoEnemyAttack()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Label3.Text += 50
+        ProgressBar2.Value += 50
+
+        Label7.Text -= 20
+        ProgressBar3.Value -= 20
+
+        Label7.Text += 20
+        ProgressBar3.Maximum += 20
+        ProgressBar3.Value += 20
+
+        DisableSkillButtons()
+        DoEnemyAttack()
+    End Sub
+
+    Sub DoEnemyAttack()
+        Dim attack As Integer = rnd.Next(1, 4)
+        Dim criticalChance As Integer = rnd.Next(1, 5)
+
+        If attack = 1 Then
+            If criticalChance = 4 Then
+                If (Label3.Text - 80) <= 0 Then
+                    Label3.Text = 0
+                    ProgressBar2.Value = 0
+                    EndBattle("Opponent")
+                    Exit Sub
+                End If
+
+                Label3.Text -= 80
+                ProgressBar2.Value -= 80
+            Else
+                If (Label3.Text - 40) <= 0 Then
+                    Label3.Text = 0
+                    ProgressBar2.Value = 0
+                    EndBattle("Opponent")
+                    Exit Sub
+                End If
+
+                Label3.Text -= 40
+                ProgressBar2.Value -= 40
+            End If
+        ElseIf attack = 2 Then
+            If criticalChance = 4 Then
+                If (Label3.Text - 180) <= 0 Then
+                    Label3.Text = 0
+                    ProgressBar2.Value = 0
+                    EndBattle("Opponent")
+                    Exit Sub
+                End If
+
+                Label3.Text -= 180
+                ProgressBar2.Value -= 180
+            Else
+                If (Label3.Text - 90) <= 0 Then
+                    Label3.Text = 0
+                    ProgressBar2.Value = 0
+                    EndBattle("Opponent")
+                    Exit Sub
+                End If
+
+                Label3.Text -= 90
+                ProgressBar2.Value -= 90
+            End If
+        ElseIf attack = 3 Then
+            If criticalChance = 4 Then
+                If (Label3.Text - 220) <= 0 Then
+                    Label3.Text = 0
+                    ProgressBar2.Value = 0
+                    EndBattle("Opponent")
+                    Exit Sub
+                End If
+
+                Label3.Text -= 220
+                ProgressBar2.Value -= 220
+            Else
+                If (Label3.Text - 110) <= 0 Then
+                    Label3.Text = 0
+                    ProgressBar2.Value = 0
+                    EndBattle("Opponent")
+                    Exit Sub
+                End If
+
+                Label3.Text -= 110
+                ProgressBar2.Value -= 110
+            End If
+        End If
+
+        NextRound()
+    End Sub
+
+    Sub NextRound()
+        Label6.Text += 1
+        EnableSkillButtons()
+
+        If Label7.Text < 30 Then
+            Button3.Enabled = False
+        End If
+
+        If Label7.Text < 50 Then
+            Button4.Enabled = False
+        End If
+
+        If Label3.Text > 850 Then
+            Button5.Enabled = False
+        End If
+    End Sub
+
+    Sub DisableSkillButtons()
+        Button2.Enabled = False
+        Button3.Enabled = False
+        Button4.Enabled = False
+        Button5.Enabled = False
+    End Sub
+
+    Sub EnableSkillButtons()
+        Button2.Enabled = True
+        Button3.Enabled = True
+        Button4.Enabled = True
+        Button5.Enabled = True
+    End Sub
+
+    Sub EndBattle(ByVal winner As String)
+        Label9.Text = $"{winner} wins!"
+    End Sub
+End Class
